15561de15a8a206b5d4c8f7b48340b9f3a2eb192
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-10-01 01:17:03 +0330

    Update main.rs

---
+++ timer_scheduler/src/main.rs
@@ -0,0 +1,40 @@
+#[macro_use]
+extern crate serde;
+extern crate diesel_migrations;
+#[macro_use]
+extern crate diesel;
+
+
+use actix_web::middleware::{DefaultHeaders, Logger};
+use actix_web::{App, HttpServer};
+use bastion::prelude::*;
+use dotenv::dotenv;
+use uuid::Uuid;
+
+#[actix_rt::main]
+async fn web_main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .data(Clients::new())
+            .wrap(DefaultHeaders::new().header("x-request-id", Uuid::new_v4().to_string()))
+            .wrap(Logger::new("IP:%a DATETIME:%t REQUEST:\"%r\" STATUS: %s DURATION:%D X-REQUEST-ID:%{x-request-id}o"))
+            .wrap(crate::todo_api_web::middleware::Authentication)
+            .configure(app_routes)
+    })
+        .workers(num_cpus::get() + 2)
+        .bind("0.0.0.0:4000")
+        .unwrap()
+        .run()
+        .await
+}
+
+#[fort::root]
+async fn main(_: BastionContext) -> Result<(), ()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+
+    dotenv().ok();
+
+    let _ = web_main();
+
+    Ok(())
+}
